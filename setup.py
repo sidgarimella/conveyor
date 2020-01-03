ae7202ec1a691b71368816ea6b20ccff4f43125b
Siddharth Garimella <gsidsid@gmail.com>
2020-01-02 23:25:41 -0500

    renaming pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="conveyor",
+    name="jupyter-conveyor",
     version="0.0.1",
     license="MIT",
     author="Siddharth Garimella",
